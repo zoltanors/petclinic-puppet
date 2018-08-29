40481651e1edfb6bf98f4b91e4b67d8998312a74
Zoltán Orsós <zoltanors@gmail.com>
2018-08-29 16:26:15 +0000

    Added petclinic module and site.pp

---
+++ modules/petclinic/manifests/init.pp
@@ -0,0 +1,42 @@
+class petclinic {
+  exec{'apt-update':
+    command => '/usr/bin/apt-get update'
+  }
+
+  $packages = ['openjdk-8-jre', 'tomcat7']
+  package { $packages:
+    require => Exec['apt-update'],
+    ensure => installed,
+  }
+
+  file {'/usr/share/tomcat7':
+    owner => 'tomcat7',
+    group => 'tomcat7',
+    ensure => directory,
+    require => Package['tomcat7'],
+  }
+
+  file {'/var/lib/tomcat7/webapps/petclinic.war':
+    owner => 'tomcat7',
+    group => 'tomcat7',
+    mode => '0644',
+    ensure => present,
+    require => Exec['download-petclinic-war']
+  }
+  exec { 'download-petclinic-war':
+    command => '/usr/bin/curl -o petclinic.war http://10.0.2.2:18081/artifactory/libs-snapshot-local/org/springframework/samples/spring-petclinic/1.0.1-SNAPSHOT/spring-petclinic-1.0.1-20180829.125504-1.war',
+    creates => '/var/lib/tomcat7/webapps/petclinic.war',
+    cwd => '/var/lib/tomcat7/webapps',
+    require => Package['tomcat7']
+  }
+  service { tomcat7:
+    ensure => running,
+    enable => true
+  }
+  exec{'add-tomcat-java-home':
+    cwd => ['/etc'],
+    path => ['/etc','/usr/bin'],
+    command => '/bin/echo "JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/" >> /etc/default/tomcat7',
+    notify => Service['tomcat7']
+  }
+}
